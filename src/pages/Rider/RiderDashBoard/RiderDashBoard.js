050e782a70f8bb2c3828278f8d2152e6640a6dc7
suffer725 <suhanasuffer74@gmail.com>
2025-07-12 11:22:50 -0400

    Fix Veloxo App Issues

---
+++ src/pages/Rider/RiderDashBoard/RiderDashBoard.js
@@ -0,0 +1,9 @@
+@@ .. @@
+       <header className="header">
+         <div className="header-content">
+           <div className="logo">
+-            <span className="logo-text">QuickCab</span>
++            <span className="logo-text">Veloxo</span>
+           </div>
+           <div className="auth-buttons">
+             <button className="login-btn-header" onClick={toggleOptions}>
